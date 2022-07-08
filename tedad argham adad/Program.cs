931e725af09c5304645b4f26bb4f72b354cfa4a0
Yamahdi@1984 <fatemeh.kamali198422@gmail.com>
2022-07-09 02:32:06 +0430

    tamrin7

---
+++ tedad argham adad/Program.cs
@@ -0,0 +1,12 @@
+﻿int num, a, count=0;
+System.Console.WriteLine("Enter a number:");
+num=int.Parse(Console.ReadLine());
+while (num != 0)
+{
+    a = num % 10 ;
+    num = num / 10 ;
+   count ++ ;
+}
+
+System.Console.WriteLine("tedad argham adad=" + count);
+ 
